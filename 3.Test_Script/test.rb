c24c367f5c2e693f0f3c908b82e10c30d1b99e1f
pepi-tsankov <pepi_tsankov@abv.bg>
2015-12-21 21:12:10 +0200

    Update test.rb

--- 3.Test_Script/test.rb
+++ 3.Test_Script/test.rb
@@ -1,1 +1,16 @@
 
+require_relative 'parser'
+
+module WordCounter
+
+  class FileParser
+  
+    def parse_file(file)
+      files = File.open(file, 'r')
+      what_is_in_the_file = files.read
+      Parser.new.parse_string what_is_in_the_file 
+    end
+    
+  end
+  
+end
